a6590cc3d3bdaeea9f5a24fcbc5a393d515b4e7c
Artur Szczypta <arturszczypta@gmail.com>
2023-04-10 22:55:57 +0200

    Properties and dealing with selling and mortgage

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-'''
-Simulating a monopoly game, to test strategies
-''' 
+''' Simulating a monopoly game, to test strategies''' 
 
 import numpy as np
 import random
-import csv
+from properties import Property, field_list
 
 ITERATIONS = 10
 #how manu TURNS there is
 TURNS = 1
 PLAYERS = 5
 FUNDS = 2000
+houses = 32
+hotels = 12
 
 #Supress scientific notation
 np.set_printoptions(suppress=True)
@@ -35,6 +35,8 @@ class Player:
         self.current_loc = 0
 
         self.properties = []
+        self.house_count = 0
+        self.hotels_count = 0
 
         self.out_of_jail_cards = {'chance': False, 'chest': False}
         self.turns_in_jail = 0
@@ -45,22 +47,6 @@ class Player:
     def details(self):
         ''' Return all player attributes as a dictionary'''
         return vars(self)
-
-    def move(self,val):
-        ''' Change location based on input (dices or cards)'''
-        self.current_loc =+ val
-        if self.current_loc > 39:
-            self.current_loc = 40 - self.current_loc
-            self.passed_start += 1
-            self.funds += 200
-
-        elif  self.current_loc < 0:
-            self.current_loc = 40 + self.current_loc
-
-        # Go to jail
-        if self.current_loc == 30:
-            self.current_loc = 10
-            self.turns_in_jail = 1
         
     def use_card(self):
         ''' Use out of jail card if avaliable'''
@@ -74,127 +60,87 @@ class Player:
             return True
         return False
 
-# Setting out of jail 
+    def buy_or_not(self):
+        ''' Decide if buy or auction'''
+        # buy = strategy(self.details, player.current_loc)
+        buy = True
+        # in future checking funds will not be necessary
+        if buy and self.funds >= field_list[self.current_loc].price:
+            self.funds -= field_list[self.current_loc].price
+            self.append(self.current_loc)
+        else:
+            return 'auction'
+        
+    def auction(self, field_number, current_bid):
+        ''' Decide if auction or fold'''
+        # decide if auction or fold
+        # auction = strategy(self.details, field_number, current_bid)
+        auction = True
+        if auction:
+            return current_bid + 1
+        else:
+            print('fold')
+
+    def pay(self, payment):
+        ''' Pay rent if player has enough funds'''
+        if self.funds >= payment:
+            self.funds -= payment
+        else:
+            while self.funds < payment:
+                # Sort first by rent, acending
+                self.properties = sorted(self.properties, key=lambda x: x.rent)
+                # mortgage one property at a time, starting from not full sets
+                for property in self.properties:
+                    if property.status == 0:
+                        self.funds += property.mortage(self)
+                        if self.funds >= payment:
+                            break
+                # if still not enough funds, mortgage utilities
+                for property in self.properties:
+                    if property.set == 'Utility':
+                        self.funds += property.mortage(self)
+                        if self.funds >= payment:
+                            break
+                # if still not enough funds, mortgage stations
+                for property in self.properties:
+                    if property.set == 'Station':
+                        self.funds += property.mortage(self)
+                        if self.funds >= payment:
+                            break
+                # if still not enough funds, sell hotels
+                for property in self.properties:
+                    if property.status == 3 and houses > 3:
+                        self.funds += property.buildinng_cost / 2
+                        property.status = 2
+                        houses -= 4
+                        hotels += 1
+                        if self.funds >= payment:
+                            break
+                # if still not enough funds, sell houses, then mortgage
+                for property in self.properties:
+                    given_set = [prop for prop in self.properties if prop.set == property.set]
+                    max_status = 2
+                    while any(prop.status > 1 for prop in given_set):
+                        for prop in given_set:
+                            if prop.status == max_status:
+                                self.funds += prop.buildinng_cost / 2
+                                prop.status -= 0.25
+                                houses += 1
+                                if self.funds >= payment:
+                                    break
+                        max_status -= 0.25
+                    # if still not enough funds, mortgage
+                    for property in given_set:
+                        self.funds += property.mortage(self)
+                        if self.funds >= payment:
+                            break
+                # if all properties mortgaged, return 'bancrupt'
+                if self.funds < payment:
+                    return 'bancrupt'
+
+
+                    
 free_jail_cards = {'chance': True, 'chest': True}
-
-#field 7,22,36
-def chance_card(player,has_jail_card=True):
-    ''' Pick chance card'''
-    if free_jail_cards['chance']:
-        card = random.randint(0,16)
-    else:
-        card = random.randint(1,16)
-    if card == 0:
-        player.out_of_jail_cards['chance'] = True
-        free_jail_cards['chance'] = False
-    elif card == 1:
-        player.current_loc = 0
-        player.funds += 200
-    elif card == 2:
-        if player.current_loc > 5:
-            player.funds += 200
-        player.current_loc = 5
-    elif card == 3:
-        if player.current_loc > 11:
-            player.funds += 200
-        player.current_loc = 11
-    elif card == 4:
-        if player.current_loc > 24:
-            player.funds += 200
-        player.current_loc = 24
-    elif card == 5:
-        player.current_loc = 39
-    elif card == 6:
-        player.current_loc = 10
-        player.turns_in_jail = 1
-    elif card == 7:
-        player.current_loc -= 3
-    elif card == 8:
-        if player.current_loc in (7, 36):
-           player.current_loc = 12
-           #buy or pay 10 times the dice thrown!!!!!!!!
-        else:
-           player.current_loc = 28
-           #buy or pay 10 times the dice throw!!!!!!!!
-    elif card == 9:
-        #nearest rail station
-        if player.current_loc == 7:
-           player.current_loc = 10
-           #buy or pay 2 times more!!!!!!!!
-        elif player.current_loc == 22:
-           player.current_loc = 25
-           #buy or pay 2 times more!!!!!!!!
-        else:
-            player.current_loc = 5
-            #buy or pay 2 times more!!!!!!!
-    elif card == 10:
-        player.funds += 150
-    elif card == 11:
-        player.funds += 100
-    elif card == 12:
-        player.funds += 50
-    elif card == 13:
-        player.funds -= 15
-    elif card == 14:
-        #Each player pays 50
-        list_temp = [x for x in player_list if x != player]
-        for players in list_temp:
-            players.funds -= 50
-            player.funds += 50
-    elif card == 15:
-        #for each house pay 25, for hotels 100
-        house_count = 0
-        hotels_count = 0
-        players.funds -= 25*house_count + 100*hotels_count
-
-#field 2,17,33
-def chest_card(player,has_jail_card=True):
-    ''' Pick chest card'''
-    if free_jail_cards('chest'):
-        card = random.randint(0,17)
-    else:
-        card = random.randint(1,17)
-    if card == 0:
-        player.out_of_jail_cards['chance'] = True
-        free_jail_cards['chance'] = False
-    elif card == 1:
-        player.current_loc = 0
-        player.funds += 200
-    elif card == 2:
-        player.current_loc = 10
-        player.turns_in_jail = 1
-    elif card == 3:
-        player.funds += 200
-    elif card in range(4,7):
-        player.funds += 100
-    elif card == 7:
-        player.funds += 50
-    elif card == 8:
-        player.funds += 25
-    elif card == 9:
-        player.funds += 20
-    elif card == 10:
-        player.funds += 10
-    elif card in range(11,14):
-        player.funds -= 50
-    elif card == 14:
-        #Each player pays 50
-        list_temp = [x for x in player_list if x != player]
-        for players in list_temp:
-            players.funds -= 50
-            player.funds += 50
-    elif card == 16:
-        #Each player pays 50
-        list_temp = [x for x in player_list if x != player]
-        for players in list_temp:
-            players.funds -= 10
-            player.funds += 10
-    elif card == 16:
-        #for each house pay 25, for hotels 100
-        house_count = 0
-        hotels_count = 0
-        players.funds -= 40*house_count + 115*hotels_count
-
 class Game:
     def __init__(self, players_count, start_funds):
         self.players_count = players_count
@@ -207,7 +153,7 @@ class Game:
             self.player_throws(player)
 
     def player_throws(self, player):
-        '''  Deals with player throwing the dice, going to and going out of jail'''
+        ''' Deals with player throwing the dice, going to and going out of jail'''
         doubles_count = 0
         while doubles_count < 3:
             throw = throw_dice()
@@ -240,3 +186,151 @@ class Game:
     def bankrupcy(self, player):
         ''' Delete player once bankrupt'''
         self.players.remove(player)
+    
+    #field 7,22,36
+    def chance_card(self, player):
+        ''' Pick chance card'''
+        if free_jail_cards['chance']:
+            card = random.randint(0,16)
+        else:
+            card = random.randint(1,16)
+        if card == 0:
+            player.out_of_jail_cards['chance'] = True
+            free_jail_cards['chance'] = False
+        elif card == 1:
+            player.current_loc = 0
+            player.funds += 200
+        elif card == 2:
+            if player.current_loc > 5:
+                player.funds += 200
+            player.current_loc = 5
+        elif card == 3:
+            if player.current_loc > 11:
+                player.funds += 200
+            player.current_loc = 11
+        elif card == 4:
+            if player.current_loc > 24:
+                player.funds += 200
+            player.current_loc = 24
+        elif card == 5:
+            player.current_loc = 39
+        elif card == 6:
+            player.current_loc = 10
+            player.turns_in_jail = 1
+        elif card == 7:
+            player.current_loc -= 3
+        elif card == 8:
+            # Utility
+            if player.current_loc in (7, 36):
+                player.current_loc = 12
+            #buy or pay 10 times the dice thrown!!!!!!!!
+            else:
+                player.current_loc = 28
+            #buy or pay 10 times the dice throw!!!!!!!!
+        elif card == 9:
+            # Rail station
+            #nearest rail station
+            if player.current_loc == 7:
+                player.current_loc = 10
+            elif player.current_loc == 22:
+                player.current_loc = 25
+                #buy or pay 2 times more!!!!!!!!
+            else:
+                player.current_loc = 5
+                #buy or pay 2 times more!!!!!!!
+        elif card == 10:
+            player.funds += 150
+        elif card == 11:
+            player.funds += 100
+        elif card == 12:
+            player.funds += 50
+        elif card == 13:
+            player.funds -= 15
+        elif card == 14:
+            #Each player pays 50
+            list_temp = [x for x in self.players if x != player]
+            for players in list_temp:
+                players.funds -= 50
+                player.funds += 50
+        elif card == 15:
+            #for each house pay 25, for hotels 100
+            house_count = 0
+            hotels_count = 0
+            players.funds -= 25*house_count + 100*hotels_count
+
+    #field 2,17,33
+    def chest_card(self, player):
+        ''' Pick chest card'''
+        if free_jail_cards['chest']:
+            card = random.randint(0,17)
+        else:
+            card = random.randint(1,17)
+        if card == 0:
+            player.out_of_jail_cards['chance'] = True
+            free_jail_cards['chance'] = False
+        elif card == 1:
+            player.current_loc = 0
+            player.funds += 200
+        elif card == 2:
+            player.current_loc = 10
+            player.turns_in_jail = 1
+        elif card == 3:
+            player.funds += 200
+        elif card in range(4,7):
+            player.funds += 100
+        elif card == 7:
+            player.funds += 50
+        elif card == 8:
+            player.funds += 25
+        elif card == 9:
+            player.funds += 20
+        elif card == 10:
+            player.funds += 10
+        elif card in range(11,14):
+            player.funds -= 50
+        elif card == 14:
+            #Each player pays 50
+            list_temp = [x for x in self.players if x != player]
+            for players in list_temp:
+                players.funds -= 50
+                player.funds += 50
+        elif card == 15:
+            #Each player pays 50
+            list_temp = [x for x in self.players if x != player]
+            for players in list_temp:
+                players.funds -= 10
+                player.funds += 10
+        elif card == 16:
+            #for each house pay 25, for hotels 100
+            house_count = 0
+            hotels_count = 0
+            players.funds -= 40 * house_count + 115 * hotels_count
+
+    def move(player, throw):
+        ''' Move player on the board'''
+        if player.current_loc + throw > 39:
+            player.funds += 200
+            player.current_loc = (player.current_loc + throw) - 40
+        
+        if player.current_loc == 4:
+            # Income Tax
+            player.funds -= 200
+        elif player.current_loc == 38:
+            # Luxury Tax
+            player.funds -= 100
+        elif player.current_loc == 30:
+            # Go to Jail
+            player.current_loc = 10
+            player.turns_in_jail = 1
+        elif player.current_loc in (2, 17, 33):
+            # Community Chest
+            player.chest_card(player)
+        elif player.current_loc in (7, 22, 36):
+            # Chance
+            player.chance_card(player)
+        else:
+            # Buy or pay rent
+            player.buy_or_pay_rent(player)
+        
+
+        
---
+++ properties.py
@@ -0,0 +1,129 @@
+import csv
+
+# Create Property class
+class Property:
+    def __init__(self, number, names, set_name, in_set, price, mortgage, rent, buildinng_cost, house_1, houses_2, houses_3, houses_4, hotel):
+        self.number = number
+        self.names = names
+        self.set = set_name
+        self.in_set = in_set
+        self.price = price
+        self.mortgage = mortgage
+        self.base_rent = rent
+        self.rent = rent
+        self.buildinng_cost = buildinng_cost
+        self.house_1 = house_1
+        self.houses_2 = houses_2
+        self.houses_3 = houses_3
+        self.houses_4 = houses_4
+        self.hotel = hotel
+        self.variant = 0 # All: 0 - not full set, 1 - full set, -1 - morgaged
+        # Station: 0 - 1 station, 0.33 - 2 stations, 0.67 - 3 stations, 1 - 4 stations
+        # Utility: 0 - 1 utility, 1 - 2 utilities
+        # Preperties: 1.25 - 1 house, 1.5 - 2 houses, 1.75 - 3 houses, 2 - 4 houses, 3 - hotel
+
+    def details(self):
+        ''' Return all property attributes as a dictionary'''
+        return vars(self)
+    
+    def buy(self, player):
+        '''Buy property if player has enough funds'''
+        if self.price <= player.funds:
+            self.owner = player.number
+            player.funds -= self.price
+    
+    def change_owner(self, player, buyer):
+        '''Change property owner'''
+        if self.owner == player.number:
+            self.owner = buyer.number
+            player.properties -= 1
+            buyer.properties += 1
+        else:
+            print('unable to change owner')
+    
+    def morgage(self, player):
+        '''Morgage property if player has enough funds'''
+        if self.variant != -1:
+            self.variant = -1
+            player.funds += self.mortgage
+        else:
+            player('unable to morgage')
+    
+    def unmorgage(self, player):
+        '''Unmorgage property if player has enough funds'''
+        if self.variant == -1 and player.funds >= self.mortgage:
+            self.variant = 0
+            player.funds -= self.mortgage
+        else:
+            print('unable to unmorgage')
+
+    def calculate_rent(self, dice_roll):
+        '''Calculate rent for property'''
+        if self.set == 'Station':
+            if self.variant == 0:
+                self.rent = 25
+            elif self.variant == 0.33:
+                self.rent = 100
+            elif self.variant == 0.67:
+                self.rent = 150
+            elif self.variant == 1:
+                self.rent = 200
+        elif self.set == 'Utility':
+            if self.variant == 0:
+                self.rent = dice_roll * 4
+            elif self.variant == 1:
+                self.rent = dice_roll * 10
+        else:
+            # Streets
+            if self.variant == 0:
+                self.rent = self.base_rent
+            elif self.variant == 1:
+                self.rent == self.base_rent * 2
+            elif self.variant == 1.25:
+                self.rent = self.house_1
+            elif self.variant == 1.5:
+                self.rent = self.houses_2
+            elif self.variant == 1.75:
+                self.rent = self.houses_3
+            elif self.variant == 2:
+                self.rent = self.houses_4
+            elif self.variant == 3:
+                self.rent = self.hotel
+        if self.variant == -1:
+                self.rent = 0
+
+def field_list(dice_roll=None):
+    '''Create list of all fields'''
+    # Create list of 40 fields
+    fields = [None] * 40
+    fields[0] = 'Start'
+    fields[10] = 'Jail'
+    fields[20] = 'Free Parking'
+    fields[30] = 'Go to Jail'
+    fields[2] = 'Community Chest'
+    fields[17] = 'Community Chest'
+    fields[33] = 'Community Chest'
+    fields[7] = 'Chance'
+    fields[22] = 'Chance'
+    fields[36] = 'Chance'
+    fields[4] = 'Income Tax'
+    fields[38] = 'Luxury Tax'
+
+    # Save properties.csv content list of dictionaries
+    with open('properties.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row_dict = {header: value for header, value in row.items()}
+            fields[int(row_dict['number'])] = Property(**row_dict)
+    return fields
+
+if __name__ == '__main__':
+    # Print all fields
+    fields = field_list()
+    for field in fields:
+        print(type(field))
+        if isinstance(field, Property):
+            print(field.details())
+        else:
+            print(field)
+        print('-' * 20)
